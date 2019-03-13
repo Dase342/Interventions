b27fe65f8772c5e723f5c95f5e10161d4e1cd0cc
david to <davidt_888@hotmail.com>
2019-03-13 15:05:11 -0400

    Zone PRÉNOM valide avec 2 espaces et 1 caractère

--- src/app/probleme/probleme.component.spec.ts
+++ src/app/probleme/probleme.component.spec.ts
@@ -59,4 +59,12 @@ describe('ProblemeComponent', () => {
     expect(zone.valid).toBeTruthy();
   });
 
+  it('Zone PRÉNOM valide avec 2 espaces et 1 caractère', () => {
+    
+    let zone = component.problemeForm.controls['prenom'];
+    zone.setValue(' '.repeat(2) + 'a');
+    
+    expect(zone.valid).toBeTruthy();
+  });
+
 });
